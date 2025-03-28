5baf4ab9cd2972d5f24d05ecd32f35f45fa38636
thecorf <mim010@edu.zealand.dk>
2025-03-28 09:32:43 +0100

    made the thing

---
+++ ConsoleApp3/MysteryBox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class MysteryBox
+    {
+        private string _label;
+        private string _contains;
+        private bool _isExplosive;
+
+        public MysteryBox(string Label, string Contains, bool IsExplosive) 
+        { 
+            _label = Label;
+            _contains = Contains;
+            _isExplosive = IsExplosive;
+        }
+
+
+        public string Label
+        {
+            get { return Label; }
+            set { Label = value; }
+        }
+
+        public string Contains
+        {
+            get { return Contains; }
+            set { Contains = value; }
+        }
+
+        public bool IsExplosive
+        {
+            get { return IsExplosive; }
+            set { IsExplosive = value; }
+        }
+
+
+
+
+    }
+}
